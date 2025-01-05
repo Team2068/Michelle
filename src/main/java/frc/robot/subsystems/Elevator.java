f3683b8b1a16539bdda39662b56f9c5f95898f3c
312Evan <emcgrael@outlook.com>
2025-01-05 15:37:46 -0500

    elevator code

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,13 +1,55 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-  /** Creates a new Elevator. */
-  public Elevator() {}
+  public SparkMax elevatorMotor = new SparkMax(0, MotorType.kBrushless);;
+  public SparkMax elevatorFollower = new SparkMax(1, MotorType.kBrushless);
+  SparkMaxConfig config = new SparkMaxConfig();
+
+  public Elevator() {
+
+
+    config
+      .follow(elevatorMotor, true)
+      .idleMode(SparkMaxConfig.IdleMode.kBrake);
+
+    config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(0, 0, 0);
+
+      elevatorMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      elevatorFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public void moveElevator(double speed) {
+    elevatorMotor.set(speed);
+  }
+  
+  public void moveElevatorVolts(double speed) {
+    elevatorMotor.setVoltage(speed);
+  }
+
+  public void stopElevator(){
+    elevatorMotor.stopMotor();
+  }
+
+  public double elevatorPos() {
+    return elevatorMotor.getEncoder().getPosition();
+  }
+
+  public void resetEncoders(){
+    elevatorMotor.getEncoder().setPosition(0);
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+
   }
 }
