8594b42e2a8b9a740518ea419ff142c772b12223
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2025-02-15 10:45:51 -0500

    Updated elevatorsim for theoretical robot parameters

--- src/main/java/frc/robot/subsystems/Simulation/MapleSim.java
+++ src/main/java/frc/robot/subsystems/Simulation/MapleSim.java
@@ -1,12 +1,5 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems.Simulation;
 
-// package frc.robot.subsystems.Simulation;
+public class MapleSim {
 
-// import org.ironmaple.simulation.SimulatedArena;
-// import org.ironmaple.simulation.seasonspecific.reefscape2025.Arena2025Reefscape;
-// /** Add your docs here. */
-// public class MapleSim {
-// public Arena2025Reefscape Arena= new Arena2025Reefscape();
-// }
+}
