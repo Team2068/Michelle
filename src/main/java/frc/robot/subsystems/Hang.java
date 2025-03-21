59b0b686d0b949dbcada2a2265910e322baf44d7
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2025-03-20 23:48:17 -0400

    HANG TIMEEE!

--- src/main/java/frc/robot/subsystems/Hang.java
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -1,18 +1,32 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hang extends SubsystemBase {
-  public TalonFX hang = new TalonFX(16, "rio"); // We Don't actually know the motor yet
-
+  public SparkMax hang = new SparkMax(16, MotorType.kBrushed);
+  TrapezoidProfile profile = new TrapezoidProfile(new Constraints(100, 500));
+  Timer time = new Timer();
+  double target = 0.0;
 
   public Hang() {
-    // TODO: Configure a soft limit switch
-    hang.setNeutralMode(NeutralModeValue.Brake);
+    SparkMaxConfig config = new SparkMaxConfig();
+
+    config.idleMode(SparkMaxConfig.IdleMode.kBrake);
+    config.closedLoop.pid(0.3, 0.0, 0.0, ClosedLoopSlot.kSlot0);
+    hang.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void hangSpeed(double speed) {
@@ -27,8 +41,15 @@ public class Hang extends SubsystemBase {
     hang.stopMotor();
   }
 
+  public double angle() {
+    return hang.getAbsoluteEncoder().getPosition();
+  }
+
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Hang Position", hang.getPosition().getValueAsDouble());
+    double cTime = time.get();
+    State out = profile.calculate(cTime, new State(angle(), 0), new State(target, 0));
+    hang.getClosedLoopController().setReference(out.position, ControlType.kPosition);
+    SmartDashboard.putNumber("Hang Pos", angle());
   }
 }
